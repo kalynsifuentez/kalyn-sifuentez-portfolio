36e8568a5fed13e9baee744469d4264e1cf92e9c
kalynsifuentez <kalynsifuentez@gmail.com>
2024-04-22 22:45:36 -0500

    updated css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 
 import App from './App';
 import Error from './pages/Error';
